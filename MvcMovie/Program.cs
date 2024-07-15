ffed981ee8f41d5d37af0a383b1114f5e6db586b
andrewtchan <andrew2020chan@gmail.com>
2024-07-15 15:34:33 -0700

    set app-level Culture

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
 using MvcMovie.Models;
+using System.Globalization;
 
 namespace MvcMovie
 {
@@ -27,8 +29,16 @@ namespace MvcMovie
             {
                 var services = scope.ServiceProvider;
 
-                var db = scope.ServiceProvider.GetRequiredService<MvcMovieContext>();
-                db.Database.Migrate();
+                // Perform db migrations
+                try
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<MvcMovieContext>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Failed to apply migrations.", ex);
+                }
 
                 SeedData.Initialize(services);
             }
@@ -40,6 +50,20 @@ namespace MvcMovie
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // configure Culture
+            var supportedCultures = new[]
+            {
+                new CultureInfo("en-US"),
+            };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                // Formatting numbers, dates, etc.
+                SupportedCultures = supportedCultures,
+                // UI strings that we have localized.
+                SupportedUICultures = supportedCultures
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();

f0e2fd2cd80b1129d03d473c2333e5d061b3e47b
andrewtchan <andrew2020chan@gmail.com>
2024-07-15 11:36:49 -0700

    programatic migrations

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -27,16 +27,19 @@ namespace MvcMovie
             {
                 var services = scope.ServiceProvider;
 
+                var db = scope.ServiceProvider.GetRequiredService<MvcMovieContext>();
+                db.Database.Migrate();
+
                 SeedData.Initialize(services);
             }
 
-                // Configure the HTTP request pipeline.
-                if (!app.Environment.IsDevelopment())
-                {
-                    app.UseExceptionHandler("/Home/Error");
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    app.UseHsts();
-                }
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
